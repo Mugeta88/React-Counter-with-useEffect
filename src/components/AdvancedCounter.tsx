da39f20ffc6952686f32b7911a8e5b1efbd51b1a
Your Name <mugeta88@gmail.com>
2026-01-11 07:42:14 -0500

    added the increment and decrement functionality

--- src/components/AdvancedCounter.tsx
+++ src/components/AdvancedCounter.tsx
@@ -1,9 +1,14 @@
 import { useState } from "react";
 
 function AdvancedCounter() {
+    const [count, setCount] = useState(0)
+
   return (
     <div>
       <h1>Advanced Counter</h1>
+
+      <button onClick={() => setCount(prev => prev - 1)}>Decrement</button>
+      <button onClick={() => setCount(prev => prev + 1)}>Increment</button>
     </div>
   );
 }
