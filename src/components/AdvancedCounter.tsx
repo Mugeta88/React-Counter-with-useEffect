faeb51bd4197d06875ddba0f87c9b607d8c4c98d
Your Name <mugeta88@gmail.com>
2026-01-11 08:15:29 -0500

    updated with a li to show the count history value

--- src/components/AdvancedCounter.tsx
+++ src/components/AdvancedCounter.tsx
@@ -2,8 +2,8 @@ import { useState, useEffect } from "react";
 
 function AdvancedCounter() {
     const [count, setCount] = useState<number>(0)
-
     const [history, setHistory] = useState<number[]>([0])
+    const [step, setStep] = useState<number>(1);
 
     useEffect(() => {
         setHistory((prevHistory) => [...prevHistory, count]);
@@ -13,10 +13,21 @@ function AdvancedCounter() {
   return (
     <div>
       <h1>Advanced Counter</h1>
+      <h2>Current Count: {count}</h2>
 
       <button onClick={() => setCount(prev => prev - 1)}>Decrement</button>
       <button onClick={() => setCount(prev => prev + 1)}>Increment</button>
 
+
+    <h3>Count History:</h3>
+    <ul>
+    {history.map((value, index) => (
+        <li key={index}>{value}</li>
+    ))}
+    </ul>
+
+
+    
       
     </div>
   );
